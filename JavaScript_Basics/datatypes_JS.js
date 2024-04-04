fd5959a867ae5fd1afb990abe8d090f70c366311
VaibhavJC <97387485+VaibhavJC@users.noreply.github.com>
2024-04-04 09:40:02 +0000

    datatypes

---
+++ JavaScript_Basics/datatypes_JS.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all code as newer version of javaScript  
+
+//alert(3 + 3) // we are using node, not browser
+
+let name = "Vaibhav";
+let age = 50;
+let loggedIn = true;
+
+//Data Types in JS
+
+//Primitive Data Types
+
+/*
+number => 2 to power 53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => value not assigned
+symbol => to find uniqueness in component (used in react)
+*/
+
+//Structured Data Types
+/*
+object => to store multiple elements in key value format
+array => to store multiple elements
+*/
