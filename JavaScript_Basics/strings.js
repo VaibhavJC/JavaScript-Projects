d1b973ec814bdf36e83fe193074479eb46e30e07
VaibhavJC <97387485+VaibhavJC@users.noreply.github.com>
2024-04-11 12:02:40 +0000

    String Exercise 2

--- JavaScript_Basics/strings.js
+++ JavaScript_Basics/strings.js
@@ -44,8 +44,69 @@ true
 false */
 
 function is_blank(str1){
-    return (str1 === '');
+    return (str1.length === 0);
 }
 
 is_blank('');
 is_blank('abc');
+
+/* 3. Write a JavaScript function to split a string and convert it into an array of words.
+Test Data :
+console.log(string_to_array("Robin Singh"));
+["Robin", "Singh"] */
+
+function string_to_array(str){
+    return str.trim().split(" ");
+}
+
+console.log(string_to_array("Robin Singh"));
+
+/*Write a JavaScript function to extract a specified number of characters from a string.
+Test Data :
+console.log(truncate_string("Robin Singh",4));
+"Robi" */
+
+function truncate_string(str, num){
+    if(str.constructor === String && (num > 0)){
+        return str.slice(0,num);
+    }
+}
+
+console.log(truncate_string("Robin Sharma", 4))
+
+/*5. Write a JavaScript function to convert a string into abbreviated form.
+Test Data :
+console.log(abbrev_name("Robin Singh"));
+"Robin S." */
+
+function abbrev_name(str){
+    let splitName = str.trim().split(" ")// ["Robin", "Sharma"];
+    if(splitName.length > 1){
+        return (`${splitName[0]} ${splitName[1].charAt(0)}.`);
+    }
+}
+
+console.log(abbrev_name("Robin Sharma"));
+
+/*6. Write a JavaScript function that hides email addresses to prevent unauthorized access.
+Test Data :
+console.log(protect_email("robin_singh@example.com"));
+"robin...@example.com" */
+
+function protect_email(str){
+    let avg, part1, part2, splitted;
+
+    splitted = str.trim().split('@');//['robin_singh', 'example.com'];
+
+    part1 = splitted[0];
+
+    avg = part1.length/2;
+
+    part1 = part1.substring(0,(part1.length - avg));
+
+    part2 = splitted[1];
+
+    return (`${part1}...@${part2}`);
+}
+
+console.log(protect_email("vjchaudhari121@gmail.com"))
